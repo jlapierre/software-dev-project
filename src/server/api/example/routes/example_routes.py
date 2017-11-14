8a67e31c2878313905236ea6d44891f0d393cea2
northcott-j <northcott.j@chisigma.co>
2017-11-14 16:53:54 -0500

    Cleaned out template code that was not relevant

--- src/server/api/example/routes/example_routes.py
+++ src/server/api/example/routes/example_routes.py
@@ -12,33 +12,10 @@ def serve_frontend(path):
     return send_from_directory(ROOT_PATH + '/client/', path)
 
 
-# Test route that has pre-filled parameters
-#@example_api.route('/api/example/test', methods=['GET'])
-def example_test():
-    return jsonify(kc.example_test())
-
-
-# Starts a torrent stream to Youtube from magnet link
-#@example_api.route('/api/start', methods=['POST'])
-def start_torrent_stream():
-    return jsonify(kc.start_torrent_stream())
-
-
-# Kills any torrent stream that is running
-#@example_api.route('/api/stop', methods=['DELETE'])
-def stop_torrent_stream():
-    return jsonify(kc.stop_torrent_stream())
-
-
 # Delegates slack hook
 @example_api.route('/api/slack_hook', methods=['POST'])
 @requires_slack_auth
 def slack_hook():
     cmds = request.form.get('text', '').split(' ')
     response_url = request.form.get('response_url', '')
-    if len(cmds) > 0 and cmds[0] == 'stop':
-        return jsonify(kc.stop_torrent_stream())
-    elif len(cmds) > 1 and cmds[0] == 'start':
-        return jsonify(kc.start_torrent_stream_delayed_response(cmds[1], response_url))
-    else:
-        return jsonify(kc.error())
+    return jsonify(kc.error())
