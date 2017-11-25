c5c5626f7272fb737ab14f07aeb90388b2013b42
northcott-j <northcott.j@chisigma.co>
2017-11-24 19:07:54 -0500

    Add ssh-agent commands to setup

--- test/start_test_env.sh
+++ test/start_test_env.sh
@@ -1,11 +1,13 @@
-ssh -tt -i ~/.ssh/cs4500-admin.pem ubuntu@128.31.25.123 << EOT
+ssh -tt -i ~/.ssh/cs4500-admin.pem ubuntu@128.31.25.123 << 'EOF'
 sudo apt-get install python-pip
 sudo apt-get install virtualenv
 sudo rm -r 108
+eval $(ssh-agent -s)
+ssh-add .ssh/github-ccs
 git clone git@github.ccs.neu.edu:CS4500/108.git
 cd 108
 git pull -u origin adding-jenkins
 git checkout adding-jenkins
 sudo pip install requirements.txt
 bash test/karma_setup.sh
-EOT
+EOF
