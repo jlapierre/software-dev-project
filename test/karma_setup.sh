1e1c557f899e01942925ad22551a4419e4dc1b12
northcott-j <northcott.j@chisigma.co>
2017-11-27 12:50:15 -0500

    Adding echo comments to show which step is being run

--- test/karma_setup.sh
+++ test/karma_setup.sh
@@ -1,6 +1,12 @@
+echo "Installing nodejs..."
 sudo apt-get install nodejs
+echo "Installing npm..."
 sudo apt-get install npm
+echo "Installing karma..."
 npm install karma --save-dev
+echo "Installing angular..."
 npm install angular angular-route angular-mocks --save-dev
+echo "Installing karma local dependencies..."
 npm install karma-jasmine jasmine-core --save-dev
+echo "Installing karma-cli..."
 sudo npm install -g karma-cli
--- test/start_test_env.sh
+++ test/start_test_env.sh
@@ -1,11 +1,17 @@
 ssh -T -o StrictHostKeyChecking=no -i ~/.ssh/cs4500-admin.pem ubuntu@128.31.25.123 <<EOSSH
+echo "Installing pip..."
 sudo apt-get install python-pip
+echo "Installing virtualenv..."
 sudo apt-get install virtualenv
+echo "Removing old 108 folder..."
 sudo rm -rf 108
+echo "Cloning 108 repo..."
 git clone git@github.ccs.neu.edu:CS4500/108.git
 cd ~/108
+echo "Pulling and checking out the new branch addition..."
 git pull origin adding-jenkins
 git checkout adding-jenkins
+echo "Installing requirements.txt..."
 sudo -H pip install -r requirements.txt
 bash ~/108/test/karma_setup.sh
 exit
