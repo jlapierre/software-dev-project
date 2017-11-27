3f196706da566119e5ae41c7660cfa806e109342
northcott-j <northcott.j@chisigma.co>
2017-11-27 12:41:02 -0500

    Fixing setup commands - had to rename .ssh key

--- test/start_test_env.sh
+++ test/start_test_env.sh
@@ -1,14 +1,12 @@
 ssh -T -o StrictHostKeyChecking=no -i ~/.ssh/cs4500-admin.pem ubuntu@128.31.25.123 <<EOSSH
 sudo apt-get install python-pip
 sudo apt-get install virtualenv
-sudo rm -r 108
-eval $(ssh-agent -s)
-ssh-add /home/ubuntu/.ssh/github-ccs
+sudo rm -rf 108
 git clone git@github.ccs.neu.edu:CS4500/108.git
 cd ~/108
 git pull origin adding-jenkins
 git checkout adding-jenkins
-sudo pip install requirements.txt
+sudo -H pip install -r requirements.txt
 bash ~/108/test/karma_setup.sh
 exit
 EOSSH
