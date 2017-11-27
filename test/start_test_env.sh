3a4663decaac3fd6ce74be001b606757a7a21338
northcott-j <northcott.j@chisigma.co>
2017-11-27 12:18:48 -0500

    Fixing the EOF for heredoc

--- test/start_test_env.sh
+++ test/start_test_env.sh
@@ -1,8 +1,8 @@
-ssh -tt -o StrictHostKeyChecking=no -i ~/.ssh/cs4500-admin.pem ubuntu@128.31.25.123 << EOF
+ssh -T -o StrictHostKeyChecking=no -i ~/.ssh/cs4500-admin.pem ubuntu@128.31.25.123 <<EOSSH
 sudo apt-get install python-pip
 sudo apt-get install virtualenv
 sudo rm -r 108
-ssh-agent -s
+eval $(ssh-agent -s)
 ssh-add /home/ubuntu/.ssh/github-ccs
 git clone git@github.ccs.neu.edu:CS4500/108.git
 cd ~/108
@@ -10,4 +10,5 @@ git pull origin adding-jenkins
 git checkout adding-jenkins
 sudo pip install requirements.txt
 bash ~/108/test/karma_setup.sh
-EOF
+exit
+EOSSH
