a5526f2e16ffa9cac106eb6ea45a62e09c063aab
northcott-j <northcott.j@chisigma.co>
2017-11-27 12:09:38 -0500

    fixing command paths and git pulls

--- test/start_test_env.sh
+++ test/start_test_env.sh
@@ -2,12 +2,12 @@ ssh -tt -o StrictHostKeyChecking=no -i ~/.ssh/cs4500-admin.pem ubuntu@128.31.25.
 sudo apt-get install python-pip
 sudo apt-get install virtualenv
 sudo rm -r 108
-eval $(ssh-agent -s)
-ssh-add .ssh/github-ccs
+ssh-agent -s
+ssh-add /home/ubuntu/.ssh/github-ccs
 git clone git@github.ccs.neu.edu:CS4500/108.git
-cd 108
-git pull -u origin adding-jenkins
+cd ~/108
+git pull origin adding-jenkins
 git checkout adding-jenkins
 sudo pip install requirements.txt
-bash test/karma_setup.sh
+bash ~/108/test/karma_setup.sh
 EOF
