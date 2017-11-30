0ea2038d2a41ea8c7c1bd2f8360d8219b865960a
liml <lim.l@husky.neu.edu>
2017-11-30 15:24:32 -0500

    use flask_cors instead of deprecated flask.ext.cors

--- src/server/api/sso/routes/sso_routes.py
+++ src/server/api/sso/routes/sso_routes.py
@@ -1,5 +1,5 @@
 from server.auth.auth_service import GOOGLE, require_access_token, get_access_token
-from flask.ext.cors import cross_origin, CORS
+from flask_cors import cross_origin, CORS
 from flask import Blueprint, session, redirect, url_for
 from server.config.private import GOOGLE_REDIRECT_URI
 
