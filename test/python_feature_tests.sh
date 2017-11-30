33dbf650673b7ef5b8c585dbd17b6d547400cb03
northcott-j <northcott.j@chisigma.co>
2017-11-30 14:26:32 -0500

    combining coverage report if lettuce tests run

--- test/python_feature_tests.sh
+++ test/python_feature_tests.sh
@@ -8,7 +8,6 @@ echo "Installing pip requirements..."
 sudo -H pip install --quiet -r requirements.txt
 echo "Starting feature tests..."
 export PYTHONPATH="${PYTHONPATH}:/home/ubuntu/108/src"
-if [ "$(find /home/ubuntu/108/test -type f -name \*.feature -type f -printf '.' | wc -c)" != "0" ]; then lettuce test; else echo "No lettuce features to run!"; fi
-coverage report
+if [ "$(find /home/ubuntu/108/test -type f -name \*.feature -type f -printf '.' | wc -c)" != "0" ]; then lettuce test && coverage report; else echo "No lettuce features to run!"; fi
 exit
 EOSSH
