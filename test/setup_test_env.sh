e1a932362d366e7edb9cc70a4ee0cd3c3a5d710f
northcott-j <northcott.j@chisigma.co>
2017-11-30 12:23:49 -0500

    Reformatted echo commands to be in the proper order

--- test/setup_test_env.sh
+++ test/setup_test_env.sh
@@ -3,9 +3,9 @@ echo "Installing pip..."
 sudo apt-get install python-pip
 echo "Installing virtualenv..."
 sudo apt-get install virtualenv
-echo "Removing old 108 folder..."
 echo "Killing any python scripts currently running..."
 sudo pkill python
+echo "Removing old 108 folder..."
 sudo rm -rf 108
 echo "Cloning 108 repo..."
 git clone git@github.ccs.neu.edu:CS4500/108.git
