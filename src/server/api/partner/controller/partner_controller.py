fb4e3462fe529d520524f473bb1e1cfba8a6fb1e
liml <lim.l@husky.neu.edu>
2017-12-02 13:34:05 -0500

    add implementation for controller api functions

--- src/server/api/partner/controller/partner_controller.py
+++ src/server/api/partner/controller/partner_controller.py
@@ -1,7 +1,18 @@
 def get_partners(database):
     """return all partners the current partner has access to"""
-    return []
+    return database["partners"].find({})
 
 
+#partner is a dictionary representing the new partner
 def upsert_partner(database, partner):
     """add or update the given partner in the database"""
+    return database["partners"].update_one({
+        "name": partner["name"]}, # find partner with matching name
+        {"$set": partner}, # update with given values
+        True # upsert mode flag
+    )
+
+
+def remove_partner(database, partner_id):
+    """delete partner with given id"""
+    return database["partners"].delete_one({"_id": partner_id})
--- src/server/api/user/controller/user_controller.py
+++ src/server/api/user/controller/user_controller.py
@@ -7,8 +7,19 @@ def get_user_with_email(database, email):
 
 def get_users(database):
     """return all users the current user has access to"""
-    return []
+    return database["users"].find({})
 
 
+#user is a dictionary representing the new user
 def upsert_user(database, user):
     """add or update the given user in the database"""
+    return database["users"].update_one(
+        {"email": user["email"]}, # find user with matching email
+        {"$set": user}, # update with given values
+        True # upsert mode flag
+    )
+
+
+def remove_user(database, user_id):
+    """delete user with given id"""
+    return database["users"].delete_one({"_id": user_id})
